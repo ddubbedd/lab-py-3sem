10cf0f1c6ddda5e686344af86ce39c3828a608af
salnyed <agorbuncova2004@gmail.com>
2022-09-25 01:00:59 +0400

    test parser

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import requests
+from bs4 import BeautifulSoup as BS
 
+r = requests.get('https://stopgame.ru/topgames?p=1')
+
+html = BS(r.content, 'html.parser')
+
+for el in html.select(".items > .item game-summary game-summary-horiz"):
+    title = el.select(".caption > .a")
+    print(title[0].text)
