0bd26736fed05878eca73488058901f1ef0c4da1
salnyed <agorbuncova2004@gmail.com>
2022-10-24 02:24:43 +0400

    fix all scripts for 2 lab

--- lab2/2_4/4.py
+++ lab2/2_4/4.py
@@ -56,7 +56,7 @@ def find_data3(date):
     else:
         return None
 
-    for i in range(0, len(list1) - 1):
+    for i in range(0, len(list1)):
         if str(date) == list1[i][0]:
             b.append(list1[i][1:])
     if not b:
@@ -69,34 +69,31 @@ def find_data4(date):
     b = []
     filename = ''
     all_files = os.listdir(path="dataset4")
-    for i in range(len(all_files) - 1):
-        if str(all_files[i])[0:4] == str(date)[0:4]:
-            print(all_files[i])
 
     if all_files:
         for file in all_files:
             if str(date)[0:4] == str(file)[0:4]:
-                if str(date)[5:6] == str(file)[5:6]:
-                    print(file)
+                if str(date)[5:7] == str(file)[4:6]:
+                    if int(file[6:8]) <= int(str(date)[8:10]) <= int(file[15:17]):
+                        filename = file
+                        break
+
+    if filename != '':
+        with open(f"dataset4/{filename}", 'r', newline='') as csvfile:
+            file_reader = csv.reader(csvfile)
+            for row in file_reader:
+                list1.append(row)
     else:
         return None
 
-    # if filename != '':
-    #     with open(f"dataset3/{filename}", 'r', newline='') as csvfile:
-    #         file_reader = csv.reader(csvfile)
-    #         for row in file_reader:
-    #             list1.append(row)
-    # else:
-    #     return None
-    #
-    # for i in range(0, len(list1)):
-    #     if str(date) == list1[i][0]:
-    #         b.append(list1[i][1:])
-    # if not b:
-    #     return None
-    # else:
-    #     return b
+    for i in range(0, len(list1)):
+        if str(date) == list1[i][0]:
+            b.append(list1[i][1:])
+    if not b:
+        return None
+    else:
+        return b
 
 
 date = datetime.date(2012, 7, 14)
-print(find_data4(date))
+print(find_data3(date))
