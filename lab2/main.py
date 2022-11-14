9491bd32bff3b8b85eb7b980dfe17be8df732259
salnyed <agorbuncova2004@gmail.com>
2022-11-14 10:46:31 +0400

    Add 5 script

---
+++ lab2/main.py
@@ -0,0 +1,13 @@
+from lab2.lab2_1 import lab1
+from lab2.lab2_2 import lab2
+from lab2.lab2_3 import lab3
+from lab2.lab2_4 import lab4
+from lab2.lab2_5 import lab5
+
+if __name__ == '__main__':
+    lab1.write_to_file()
+    lab2.write_to_file()
+    lab3.write_to_file()
+    lab4.run()
+    lab5.run()
+
